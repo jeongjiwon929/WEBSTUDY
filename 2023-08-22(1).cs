7cb8fa88514d8d68b3d7d7b7301a6c4bb53ad52d
지원 <88700479+zi-won@users.noreply.github.com>
2023-08-22 21:32:31 +0900

    c# 활용한 웹

---
+++ 2023-08-22(1).cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // args[0]
+            Console.WriteLine("당신의 이름은 "  + args[0] + "입니다");
+        }
+    }
+}
